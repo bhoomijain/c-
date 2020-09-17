b072f585460bc389ac1a1d13748acf5a020d9b7b
bhoomijain <69758906+bhoomijain@users.noreply.github.com>
2020-09-17 07:06:09 -0700

    created product.cpp
    This program finds the product of two decimal numbers!!

---
+++ product.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    double n1,n2,product;
+    cout<<"Enter two numbers:"<<endl;
+    cin>>n1>>n2;
+    product=n1*n2;
+    cout<<"Product="<<product;
+    
+
+
+    return 0;
+}
